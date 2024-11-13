3957ff894b170f915febd3a34a6ceb1db1286f71
Nicolas Sobral Cruz <nicolasunif@gmail.com>
2024-11-13 20:42:09 -0300

    Desenvolcimento de uma view de login e autenticação fake por cookie implementada

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 import os
 app = Flask(__name__)
 
 @app.route('/')
 def indexPage():
-    return('<h1> Bem vindo ao servidor! </h1> <hr> <p><a href="./create">Criar usuário</a> </p>')
+    return('<h1> Bem vindo ao servidor! </h1> <hr> <p><a href="./create">Criar usuário</a> </p><br><p><a href="./login">Fazer Login</a></p>')
 
 @app.route('/create', methods=['GET', 'POST'])
 def userCreatePage():
@@ -18,14 +18,50 @@ def userCreatePage():
         user = User(name=name, email=email, password=password)
         db.session.add(user)
         db.session.commit()
-        return('<h1> Usuário Cadastrado com Sucesso </h1><hr><p><a href="./create">Realizar outro cadastro</a></p>'),201
+        return make_response(f'<h1> Usuário {user.name} Cadastrado com Sucesso </h1><hr><p><a href="./create">Realizar outro cadastro</a></p>'),201
 
+@app.route('/login',methods=['GET', 'POST'])
+def loginPage():
+    user_cookie = request.cookies.get('user')
+    if request.method=="GET":
+        if not user_cookie:
+        #red = redirect(url_for('logoutPage'))
+        #return red, 302
+            return('<h1>Fazer Login</h1> <hr> <form action="/login" method="POST"><label>Email:</label><input type="text" name="email"><br><label>Senha:</label><input type="password" name="password"><br><button type="submit">Login</button></form>'),200
+        else:
+            red = redirect(url_for('logoutPage'))
+            return red, 302
+    elif request.method=="POST":
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first_or_404(description=f'Email não cadastrado')
+        user_password=user.password
+        #user_cookie = request.cookies.get('user')
+        if password==user_password:
+            user_id = redirect(url_for('logoutPage'))
+            user_id.set_cookie('user', str(user.id)) 
+            return user_id, 302
+        else:
+            return f'<h1>Credenciais não batem</h1><hr><p><a href="./login">Tentar Novamente</a></p>',401
+    
+        #red = redirect(url_for('logoutPage'))
+        #return red, 302
+           
+@app.route('/logout',methods=['GET', 'POST'])
+def logoutPage():
+    if request.method=="POST":
+        res = make_response("Logout done")
+        res.set_cookie('user', 'qualquer_coisa', max_age=0)
+        return res
+    else:
+        user_cookie = request.cookies.get('user')
+        k=make_response (f'<h1>Logado como { user_cookie }</h1><hr><form action="/logout" method="POST"><button type="submit">LogOut</button></form>') 
+        return k,200
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
