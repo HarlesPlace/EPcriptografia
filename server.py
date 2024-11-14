9271db588eb5b3e6224cdeab01ecc0e7c3a3b19d
Nicolas Sobral Cruz <nicolasunif@gmail.com>
2024-11-14 16:10:13 -0300

    Adequações conforme o enunciado quanto aos códigos de resposta das páginas, acrescimo de uma homepage

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, make_response, redirect,url_for
+from flask import Flask, request, make_response, redirect,url_for, abort
 from flask_sqlalchemy import SQLAlchemy
 import os
 app = Flask(__name__)
 
 @app.route('/')
 def indexPage():
-    return('<h1> Bem vindo ao servidor! </h1> <hr> <p><a href="./create">Criar usuário</a> </p><br><p><a href="./login">Fazer Login</a></p>')
+    return('<h1> Bem vindo ao servidor! </h1> <hr> <p><a href="./create">Criar usuário</a> </p><br><p><a href="./login">Fazer Login</a></p> <hr> <p><a href="./home">Página Principal</a> </p>')
 
 @app.route('/create', methods=['GET', 'POST'])
 def userCreatePage():
@@ -25,38 +25,43 @@ def loginPage():
     user_cookie = request.cookies.get('user')
     if request.method=="GET":
         if not user_cookie:
-        #red = redirect(url_for('logoutPage'))
-        #return red, 302
             return('<h1>Fazer Login</h1> <hr> <form action="/login" method="POST"><label>Email:</label><input type="text" name="email"><br><label>Senha:</label><input type="password" name="password"><br><button type="submit">Login</button></form>'),200
         else:
-            red = redirect(url_for('logoutPage'))
+            #Se usuário tentar voltar na página de login e já estiver logado ele será redirecionado
+            red = redirect(url_for('homePage'))
             return red, 302
     elif request.method=="POST":
         email = request.form['email']
         password = request.form['password']
         user = User.query.filter_by(email=email).first_or_404(description=f'Email não cadastrado')
         user_password=user.password
-        #user_cookie = request.cookies.get('user')
         if password==user_password:
-            user_id = redirect(url_for('logoutPage'))
+            user_id = redirect(url_for('homePage'))
             user_id.set_cookie('user', str(user.id)) 
             return user_id, 302
         else:
             return f'<h1>Credenciais não batem</h1><hr><p><a href="./login">Tentar Novamente</a></p>',401
-    
-        #red = redirect(url_for('logoutPage'))
-        #return red, 302
            
 @app.route('/logout',methods=['GET', 'POST'])
 def logoutPage():
     if request.method=="POST":
         res = make_response("Logout done")
         res.set_cookie('user', 'qualquer_coisa', max_age=0)
-        return res
+        return res,200
     else:
         user_cookie = request.cookies.get('user')
-        k=make_response (f'<h1>Logado como { user_cookie }</h1><hr><form action="/logout" method="POST"><button type="submit">LogOut</button></form>') 
+        user = User.query.filter_by(id=user_cookie).first()
+        k=make_response (f'<h1>Logado como { user.name }</h1><hr><form action="/logout" method="POST"><button type="submit">LogOut</button></form>') 
         return k,200
+
+@app.route('/home')      
+def homePage():
+    user_cookie = request.cookies.get('user')
+    if user_cookie:
+        user = User.query.filter_by(id=user_cookie).first()
+        return (f'<h1>Usuário {user.name} logado!</h1>')
+    else:
+        abort(401)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
@@ -68,6 +73,5 @@ class User(db.Model):
     name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
-
     def __repr__(self):
         return '<User %r>' % self.name
